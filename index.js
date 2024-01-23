68a0a724bbdf7d7787e5a5a7a041b97880ebbbef
tim <timdesmet00@gmail.com>
2024-01-23 10:09:49 +0100

    controle d'hier et acces au json

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import userRoutes from "./routes/userRoutes.js";
+
+const app = express();
+
+app.use(express.json());
+
+userRoutes(app);
+
+const PORT = 5000;
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Something went wrong");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
