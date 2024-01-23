5b1960d1a4742c50c323587bfdc239d8b6e2f63f
tim <timdesmet00@gmail.com>
2024-01-23 11:59:35 +0100

    mise en place de la fonction PUT

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -20,7 +20,7 @@ export default function (app) {
       // add new user
       content.push({ email, username });
 
-      console.log("added " + email + "to DB");
+      console.log("added " + email + " to DB");
 
       // jsonfile writing
       jsonfile.writeFile(file_path, content, function (err) {
@@ -36,8 +36,35 @@ export default function (app) {
     jsonfile.readFile(file_path, function (err, content) {
       for (let i = 0; i < content.length; i++) {
         if (content[i].email === email) {
-          console.log("removing" + content[i].email + "from DB");
+          console.log("removing" + content[i].email + " from DB");
           content.splice(i, 1);
+          break;
+        }
+      }
+      jsonfile.writeFile(file_path, content, function (err) {
+        console.log(err);
+        res.send({ status: "200", data: req.body });
+      });
+    });
+  });
+
+  app.put("/users", (req, res) => {
+    let user;
+    let username = req.body.username;
+    let email = req.query.email;
+
+    jsonfile.readFile(file_path, function (err, content) {
+      for (let i = 0; i < content.length; i++) {
+        if (content[i].email === req.query.email) {
+          console.log(
+            "updated user " +
+              req.query.email +
+              " has now username : " +
+              username
+          );
+
+          user = content[i];
+          user.username = username;
           break;
         }
       }
