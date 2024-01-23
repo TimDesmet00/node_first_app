5f4ee58a365a961c35bcaf27e9631bfd52308dc9
tim <timdesmet00@gmail.com>
2024-01-23 10:54:36 +0100

    mise en place de la route post

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ userRoutes(app);
 
 const PORT = 5000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).send("Something went wrong");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,32 @@
 import jsonfile from "jsonfile";
 
 export default function (app) {
+  const file_path = "./DB/users.json";
+
   app.get("/users", (req, res) => {
     console.log("fetching all users");
 
     // jsonfile reading
-    jsonfile.readFile("./DB/users.json", function (err, content) {
+    jsonfile.readFile(file_path, function (err, content) {
       // send file contents back to sender
       res.send(content);
+    });
+  });
+
+  app.post("/users/new", (req, res) => {
+    let { email, username } = req.body;
+    // jsonfile reading
+    jsonfile.readFile(file_path, function (err, content) {
+      // add new user
+      content.push({ email, username });
+
+      console.log("added " + email + "to DB");
+
+      // jsonfile writing
+      jsonfile.writeFile(file_path, content, function (err) {
+        console.log(err);
+        res.send({ status: "200", data: req.body });
+      });
     });
   });
 }
